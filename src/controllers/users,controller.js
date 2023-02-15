4291a576ecfb8998ee56b8d9fd4a4c3943b7f352
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2023-02-15 18:06:31 -0300

    create

--- src/controllers/users,controller.js
+++ src/controllers/users,controller.js
@@ -1,1 +1,8 @@
 import User from "../models/Users";
+
+export const createUser = async (req, res) => {
+  const user = User(req.body);
+  const userCreated = await user.save();
+  console.log(userCreated);
+  res.redirect("/tableComplete");
+};
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { createUser } from "../controllers/users,controller";
 
 //  Router
 const router = Router();
@@ -6,5 +7,7 @@ const router = Router();
 router.get("/register", (req, res) => {
   res.render("register");
 });
+
+router.post("/users/create", createUser);
 
 export default router;
